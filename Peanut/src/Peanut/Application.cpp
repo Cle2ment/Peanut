7b31080dcac64dd42e3c22bfaede239a20a48142
Clément CHEN <yikun.chen@163.com>
2026-06-06 17:09:20 +0800

    deps: upgrade ImGui to ocornut/imgui:docking (v1.91+)
    - Switch vendor/imgui submodule from TheCherno/imgui@1a96ac38
      to ocornut/imgui:docking@2af6dd96 (3,754 commits forward)
    - Adapt Application.cpp to ImGui 1.91+ API:
      - InitInfo: Subpass/MSAASamples/RenderPass → PipelineInfoMain
      - ImGui_ImplVulkan_Init() now takes single arg (RenderPass via PipelineInfoMain)
      - Font loading moved before Init() (required by 1.91+)
      - Removed obsolete CreateFontsTexture/DestroyFontUploadObjects (handled internally)
      - CreateOrResizeWindow: added VkImageUsageFlags parameter
    - No source file changes needed in xmake.lua

--- Peanut/src/Peanut/Application.cpp
+++ Peanut/src/Peanut/Application.cpp
@@ -248,7 +248,7 @@ static void SetupVulkanWindow(ImGui_ImplVulkanH_Window* wd, VkSurfaceKHR surface
 
 	// Create SwapChain, RenderPass, Framebuffer, etc.
 	IM_ASSERT(g_MinImageCount >= 2);
-	ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, wd, g_QueueFamily, g_Allocator, width, height, g_MinImageCount);
+	ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, wd, g_QueueFamily, g_Allocator, width, height, g_MinImageCount, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
 }
 
 static void CleanupVulkan()
@@ -474,49 +474,21 @@ namespace Peanut {
 		init_info.Queue = g_Queue;
 		init_info.PipelineCache = g_PipelineCache;
 		init_info.DescriptorPool = g_DescriptorPool;
-		init_info.Subpass = 0;
+		init_info.PipelineInfoMain.RenderPass = wd->RenderPass;
+		init_info.PipelineInfoMain.Subpass = 0;
 		init_info.MinImageCount = g_MinImageCount;
 		init_info.ImageCount = wd->ImageCount;
-		init_info.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+		init_info.PipelineInfoMain.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
 		init_info.Allocator = g_Allocator;
 		init_info.CheckVkResultFn = check_vk_result;
-		ImGui_ImplVulkan_Init(&init_info, wd->RenderPass);
 
-		// Load default font
+		// Load default font BEFORE backend init (ImGui 1.91+ requires this)
 		ImFontConfig fontConfig;
 		fontConfig.FontDataOwnedByAtlas = false;
 		ImFont* robotoFont = io.Fonts->AddFontFromMemoryTTF((void*)g_RobotoRegular, sizeof(g_RobotoRegular), 20.0f, &fontConfig);
 		io.FontDefault = robotoFont;
 
-		// Upload Fonts
-		{
-			// Use any command queue
-			VkCommandPool command_pool = wd->Frames[wd->FrameIndex].CommandPool;
-			VkCommandBuffer command_buffer = wd->Frames[wd->FrameIndex].CommandBuffer;
-
-			err = vkResetCommandPool(g_Device, command_pool, 0);
-			check_vk_result(err);
-			VkCommandBufferBeginInfo begin_info = {};
-			begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-			begin_info.flags |= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-			err = vkBeginCommandBuffer(command_buffer, &begin_info);
-			check_vk_result(err);
-
-			ImGui_ImplVulkan_CreateFontsTexture(command_buffer);
-
-			VkSubmitInfo end_info = {};
-			end_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-			end_info.commandBufferCount = 1;
-			end_info.pCommandBuffers = &command_buffer;
-			err = vkEndCommandBuffer(command_buffer);
-			check_vk_result(err);
-			err = vkQueueSubmit(g_Queue, 1, &end_info, VK_NULL_HANDLE);
-			check_vk_result(err);
-
-			err = vkDeviceWaitIdle(g_Device);
-			check_vk_result(err);
-			ImGui_ImplVulkan_DestroyFontUploadObjects();
-		}
+		ImGui_ImplVulkan_Init(&init_info);
 	}
 
 	void Application::Shutdown()
@@ -580,7 +552,7 @@ namespace Peanut {
 				if (width > 0 && height > 0)
 				{
 					ImGui_ImplVulkan_SetMinImageCount(g_MinImageCount);
-					ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, &g_MainWindowData, g_QueueFamily, g_Allocator, width, height, g_MinImageCount);
+					ImGui_ImplVulkanH_CreateOrResizeWindow(g_Instance, g_PhysicalDevice, g_Device, &g_MainWindowData, g_QueueFamily, g_Allocator, width, height, g_MinImageCount, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
 					g_MainWindowData.FrameIndex = 0;
 
 					// Clear allocated command buffers from here since entire pool is destroyed
